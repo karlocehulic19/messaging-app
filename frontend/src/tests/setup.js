969cda70d424ed1d6f460afb66e20f6d3156503c
Karlo Cehulic <karlocehlic@gmail.com>
2025-02-22 20:06:32 +0100

    fix: console log and error mocked in all tests

--- frontend/src/tests/setup.js
+++ frontend/src/tests/setup.js
@@ -1,10 +1,12 @@
-import { expect, afterEach } from "vitest";
+import { expect, afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom";
 import { server } from "../mocks/node";
 import "blob-polyfill";
 
 expect.extend(matchers);
+vi.spyOn(console, "log").mockImplementation(() => undefined);
+vi.spyOn(console, "error").mockImplementation(() => undefined);
 
 beforeAll(() => {
   server.listen();
