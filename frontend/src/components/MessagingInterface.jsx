7885587b92ee348a2912c036e1e6556d5ff97f2f
Karlo Cehulic <karlocehlic@gmail.com>
2025-03-29 09:01:56 +0100

    Add styling to messaging interface

--- frontend/src/components/MessagingInterface.jsx
+++ frontend/src/components/MessagingInterface.jsx
@@ -1,18 +1,33 @@
 import PropTypes from "prop-types";
 import { useProfilePicture } from "../hooks/useProfilePicture";
+import SendIcon from "../icons/send-button.svg?react";
+import styles from "./styles/MessagingInterface.module.css";
 
 export default function MessagingInterface({ receiverUsername }) {
   const profilePictureSrc = useProfilePicture(receiverUsername);
 
   return (
     <>
-      <h1>{receiverUsername}</h1>
-      <img
-        src={profilePictureSrc}
-        alt={`${receiverUsername}'s profile picture`}
-      />
-      <input type="text" aria-label="Message input" />
-      <button aria-label="Send button"></button>
+      <header id={styles["receiver-header"]}>
+        <img
+          className={styles["receiver-img"]}
+          src={profilePictureSrc}
+          alt={`${receiverUsername}'s profile picture`}
+        />
+        <h1>{receiverUsername}</h1>
+      </header>
+      <div id={styles["messaging-container"]}>
+        <div id={styles["msg-action-container"]}>
+          <input
+            id={styles["message-input"]}
+            type="text"
+            aria-label="Message input"
+          />{" "}
+          <button id={styles["send-button"]} aria-label="Send button">
+            <SendIcon className={styles["send-icon"]} />
+          </button>
+        </div>
+      </div>
     </>
   );
 }
